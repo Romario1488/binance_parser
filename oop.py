1607dedcd4e0fc17dfd9a3792d4f9acd5dabc1b2
Romario1488 <ferma3csgo@gmail.com>
2022-06-30 20:05:11 +0300

    First commit

--- oop.py
+++ oop.py
@@ -1,84 +1,66 @@
 import requests
-
-
+import json
 
 class Binance:
     def showOrder(self):
-        orders_info, dealler_id = self.__getOrders()
-        result = self.__getUserStat(orders_info, dealler_id)
-        with open('Orders.txt', 'a', encoding='utf-8') as orders:
-            for el in result:
-                print(el)
-                orders.write('\n'.join(el)+'\n\n')
+        parsed_orders = self.__getOrders()
+        parsed_sellers_rating = self.__getSellerRating(parsed_orders)
+        summary = self.__getSellerStat(parsed_sellers_rating)
+        with open('orders.txt', 'w', encoding='utf-8') as w_orders:
+            w_orders.write(json.dumps(summary))
 
-    def parseOrders(self):
+    def parseOrders(self, response):
+        all_orders = {}
+        for order in response['data']:
+
+            if order['advertiser']['monthFinishRate'] >= 0.3:
+                price = order['adv']['price']
+                id = order['advertiser']['userNo']
+                all_orders[id] = {"method": order['adv']['tradeMethods'][0]['tradeMethodName'],
+                                  'rate': str(price) + ' ' + order['adv']['fiatUnit'],
+                                  'nickName': order['advertiser']['nickName'],
+                                  'seller_link': f"https://p2p.binance.com/ru/advertiserDetail?advertiserNo={id}"}
+        return all_orders
+
+    def __getSellerRating(self, all_orders):
+        for seller_id in all_orders:
+            r = requests.post(f"https://p2p.binance.com/bapi/c2c/v1/friendly/c2c/review/user-review-statistics",
+                              json={"userNo": seller_id},
+                              headers={'User-agent': 'your bot 0.1'})
+            response_dealler_rating = r.json()
+            self.__sellerRatingParse(response_dealler_rating, seller_id, all_orders)
+        return all_orders
+
+    def __sellerRatingParse(self, response_dealler_stat, seller_id, all_orders):
+        all_orders[seller_id].update({"negativeCount": response_dealler_stat['data']['negativeCount'],
+                                      "positiveCount: ": response_dealler_stat['data']['positiveCount']})
+        return
+
+    def __getSellerStat(self, all_orders):
+        for seller_id in all_orders:
+            r = requests.get(
+                f"https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/user/profile-and-ads-list?userNo={seller_id}",
+                headers={'User-agent': 'your bot 0.1'})
+
+            response_dealler_stat = r.json()
+            self.__sellerStatParse(response_dealler_stat, seller_id, all_orders)
+        return all_orders
+
+    def __sellerStatParse(self, response_dealler_stat, seller_id, all_orders):
+        all_orders[seller_id].update({
+            "avgTransferTime": int(response_dealler_stat['data']['userDetailVo']['userStatsRet'][
+                                       'avgReleaseTimeOfLatest30day']),
+            "registerDays: ": response_dealler_stat['data']['userDetailVo']['userStatsRet']['registerDays']})
+        return
+
+    def __getOrders(self):
         r = requests.post('https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
                           json={"page": 1, "rows": 20, "publisherType": "merchant",
                                 "asset": "USDT", "tradeType": "BUY", "fiat": 'RUB'},
                           headers={'User-agent': 'your bot 0.1'})
         response = r.json()
-        return response
-
-    def __getOrders(self):
-        get_methods = self.parseOrders()
-        trade_methods_name = []
-        dealler_id = []
-        orders = []
-        rate = []
-        for order in get_methods['data']:
-            if order['advertiser']['monthFinishRate'] == 1.0:
-                dealler_id.append(order['advertiser']['userNo'])
-                trade_methods_name.append(order['adv']['tradeMethods'][0]['tradeMethodName'])
-                price = order['adv']['price']
-                rate.append(str(price) + ' ' + order['adv']['fiatUnit'])
-        i = 0
-
-        deallers = []
-        for id in dealler_id:
-            r = requests.get(f"https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/user/profile-and-ads-list?userNo={id}",
-                             headers={'User-agent': 'your bot 0.1'})
-            response = r.json()
-            deallers.append(response)
-
-        i = 0
-        for order in deallers:
-            current_order = [
-                "method: " + trade_methods_name[i],
-                'rate: ' + rate[i],
-                "nickName: " + order['data']['userDetailVo']['nickName'],
-                "link: " + 'https://p2p.binance.com/ru/advertiserDetail?advertiserNo=' + order['data']['userDetailVo'][
-                    'userNo']]
-            i += 1
-            orders.append(current_order)
-        return orders, dealler_id
-
-    def __getUserStat(self, orders, dealler_id):
-        dealler_info = []
-        for id in dealler_id:
-            r = requests.get(f"https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/user/profile-and-ads-list?userNo={id}",
-                             headers={'User-agent': 'your bot 0.1'})
-            response = r.json()
-            dealler_info.append(response)
-
-        i = 0
-        for order in orders:
-            print(order)
-            r = requests.post(f"https://p2p.binance.com/bapi/c2c/v1/friendly/c2c/review/user-review-statistics",
-                              json={"userNo": dealler_id[i]},
-                              headers={'User-agent': 'your bot 0.1'})
-            response_dealler_stat = r.json()
-            current_seller = [
-                "negativeCount: " + str(response_dealler_stat['data']['negativeCount']),
-                "positiveCount: " + str(response_dealler_stat['data']['positiveCount']),
-                "avgTransferTime: " + str(int(
-                    dealler_info[i]['data']['userDetailVo']['userStatsRet']['avgReleaseTimeOfLatest30day'])),
-                "registerDays: " + str(dealler_info[i]['data']['userDetailVo']['userStatsRet']['registerDays'])]
-
-            orders[i] += current_seller
-
-            i += 1
-
-        return orders
+        parsed_orders = self.parseOrders(response)
+        return parsed_orders
 
 
 if __name__ == '__main__':
